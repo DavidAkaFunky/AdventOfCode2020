52b09a518ed8825e2a62d86462115813246ebb02
FunkyCracky <futebol4@hotmail.com>
2020-12-01 16:40:26 +0000

    Day 1 done

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -1,24 +1,19 @@
-def part1(file):
-    f = open(file)
-    input = f.readlines()
+def part1(input):
     for i in input:
         for j in input:
-            i = int(i)
-            j = int(j)
-            if i != j and i + j == 2020:
+            if i + j == 2020:
                 return i * j
 
-def part2(file):
-    f = open(file)
-    input = f.readlines()
+def part2(input):
     for i in input:
         for j in input:
             for k in input:
-                i = int(i)
-                j = int(j)
-                k = int(k)
-                if i != j and i != k and j != k and i + j + k == 2020:
+                if i + j + k == 2020:
                     return i * j * k
-            
-print(part1("input.txt"))
-print(part2("input.txt"))
+
+f = open("input.txt")
+input = f.readlines()
+for i in range(len(input)):
+    input[i] = int(input[i])
+print(part1(input))
+print(part2(input))
