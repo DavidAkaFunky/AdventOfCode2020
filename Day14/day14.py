8095b86898a79d2967ec29b7d027e01531ea507c
FunkyCracky <futebol4@hotmail.com>
2020-12-17 18:28:32 +0000

    Day 14!

--- Day14/day14.py
+++ Day14/day14.py
@@ -24,18 +24,35 @@ def part1(input) -> int:
 
 def part2(input) -> int:
     dic = {}
-    indexes = []
     for line in input:
         line = line.split()
         line.pop(1)
         key = line[0]
         value = line[1]
         if key == "mask":
-            key = bin(int(key))[2:]
-            key = list((36-len(key)) * "0" + key)
+            count = 0
+            Xplaces = []
+            for i in range(len(value)):
+                if value[i] == "X":
+                    count += 1
+                    Xplaces.append(i)
+            mask = list(value)
+            for place in Xplaces:
+                mask[place] = "0"
+            mask = "".join(mask)
+            
         else:
-            for index in indexes:
-                dic[index] = int(value)
+            index = int(key[4:-1])
+            index = bin(index | int(mask,2))[2:]
+            indexList = list((36-len(index)) * "0" + index)
+            for i in range(2**count):
+                binI = bin(i)[2:]
+                binI = (count-len(binI)) * "0" + binI
+                for j in range(count):
+                    indexList[Xplaces[j]] = binI[j]
+                index = "".join(indexList)
+                dic[int(index,2)] = int(value)
+
     total = 0
     for el in dic:
         total += dic[el]
@@ -45,4 +62,4 @@ def part2(input) -> int:
 f = open("input.txt", "r")
 input = f.read().splitlines()
 print(part1(input)) #16003257187056
-#print(part2(input))
+print(part2(input)) #3219837697833
